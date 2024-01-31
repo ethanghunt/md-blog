a62446d7f4e3c42d0316253aeeef32bea3c25efe
ethanghunt <ethangh@gmail.com>
2024-01-31 00:21:15 -0500

    structured blogs + md test file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
 import expressLayouts from 'express-ejs-layouts';
+import showdown, { Converter } from 'showdown';
+import fs from 'fs';
+
 
 dotenv.config();
 
@@ -17,6 +20,55 @@ app.use(express.static('./src/public'));
 // Setting the root path for views
 app.set('views', './src/views');
 
+// Pulling HTML from markdown
+let converter: Converter = new showdown.Converter();
+
+let markdown: string = fs.readFileSync('./src/public/data/test.md', 'utf8');
+let html: string = converter.makeHtml(markdown);
+
+
+// Manually setting blog data
+type Blog = {
+  id: number;
+  title: string;
+  sections: { header: string, content: string }[];
+};
+let blogs: Blog[] = [{
+  id: 1,
+  title: "My First Blog",
+  sections: [
+    { header: "First Section", content: "This is the first section of my blog." },
+    { header: "Second Section", content: "This is the second section of my blog." }
+  ]
+}, {
+  id: 2,
+  title: "My Second Blog",
+  sections: [
+    { header: "First Section", content: "This is the first section of my second blog." },
+    { header: "Second Section", content: "This is the second section of my second blog." }
+  ]
+}, {
+  id: 3,
+  title: "The Skills Needed by ICU Nurses:  A Primer on What We Really Need to Know",
+  sections: [
+    { header: "", content: "As an ICU nurse, I believe I’m paid to think. Otherwise, soon, a robot could do my job. The reality is that I am the person ensuring my patients survive the twelve hours of my shift. I notice the subtle changes that sound my nurse alarm far before any electronic monitor can. However, many days, I think there are several skills truly overlooked in nursing school." },
+    { header: "Untying Knots", content: "In nursing school, I never thought for a minute that I would spend a large amount of my time untangling cords. The spaghetti mess created by the intertwined and jumbled cables, IV tubing, suction tubing, and more is overwhelming. Add to the monster-of-a-mess a patient’s cell phone charger and the call bell button, and now even attempting to turn the patient is a disaster waiting to happen.\nI can spend 15–20 precious minutes carefully untangling the pile of lines — IV tubing laid so nicely on top of everything and pulse oximeter perfectly set free only to have my patient (always connected to pressure tubing) indicate an urgent need to go to the bedside commode. How a stand and a 180 degree pivot can undo all of my hard work still continues to baffle me." },
+    { header: "The Limbo", content: "Before every ICU shift, there should be a period of stretching. As soon as I finish getting report on my new patients, I’m sure to get a call that it is time to transport my patient for a CT scan. I limbo under the IV tubing to unplug the pump. A little side limbo under the suction tubing for the ever-popular attempt to attach the IV pumps to the bed pole. Then, a precarious lean to unplug the bed and to fish the call bell button out of the cranny. Add a squeeze back to the end of the bed, and it is time to roll. With a helpful respiratory therapist towing the “portable” vent and a transport professional that makes driving the monster-of-a-bed look easy, I perform the most magnificent limbo under the computer, just squeezing by with the balloon pump. Here we go!" },
+    { header: "Squats", content: "Any day with a CRRT (continuous renal replacement therapy) machine leaves me feeling sorer and stronger than after any day at the gym! The bags seem to get heavier and heavier as the hours roll forward through the shift. Lift the bag. Squeeze the bag. Feel the satisfying pop. Mix the solution. Squat. Remove the empty bag. Disconnect the line. Hang the bag. (Why are the plastic grommets so hard to penetrate?) Reconnect the line. Stand. Repeat. Repeat. Repeat…"},
+    { header: "Reframing — The Realists", content: "Sometimes doctors, physician’s assistants, and nurse practitioners in the ICU setting tend to be specialized and caring for dozens of patients on a given day. They are amazing professionals but are often looking at a somewhat narrow and hurried picture of a patient. They do not spend hours and hours with any given patient in the same way as an ICU nurse.\nInto the patient’s room, enter the nephrologist, “The kidney numbers are improving.” Then, the surgeon, “The surgery site is healing nicely.” And infectious disease, “The white blood cells are trending down.” Everyone walks out, and the nurse, hanging the fourth IV pressor as the patient continues to decline, “I don’t think your loved one is going to make it through the day. Let’s talk about what we just heard.”"},
+    { header: "Hermione’s Time Turner", content: "Twelve hours. Go! Two patients. One vent. One CRRT machine. One balloon pump. Eight IV drips. One feeding tube. One walking-talking patient. Six rounds of diarrhea requiring three complete linen changes. One bath. Three dressing changes. Assist with a central line insertion. A trip to CT. Three calls from family. Feeding one patient. One sheath removed — hold pressure for 30 minutes. No, make that 60 minutes. Wait, 85 minutes. Up to the commode. New policy, must stay within arm’s length. Code blue across the hall. Do I leave patient on commode? No, stay. Making a mental list, it’s been three and a half hours since I turned my other patient. I don’t think this arms-length policy is going to work. Help patient back to bed. Wish I could untangle the cords — not now. Find the right TV channel. Plug the patient’s phone charger in. Remember my meds are late.\nCheck on code. Answer call bell for nurse in the code. Crush meds. Answer phone. Look for provider. Report lab value. Try to draw labs. Fail. Call phlebotomy. Get interrogated — did I ask another nurse to try — well no, they are still in a code. Family in the hall, I forgot the ice water. Family on the phone — they’ve been calling for 20 minutes. I can’t find their nurse. She’s doing CPR. Did I give those meds? Yes, I can call x-ray. The order’s not right. I can fix that. Patient needs to poop again. I still didn’t turn my other patient. Only 3 hours to go and not a single thing documented yet, and did I turn my patient?"},
+    { header: "Shake It Off", content: "At 6:00 my patient died. I cried with his wife as I hugged her neck. I carefully put his body into the body bag and called the morgue. We took care of him for three months. There was nothing more to do that would help\nAt 8:00 I walk in the door at home and shower. My family asks the question to determine how may day was, “How many poops did you clean today, mom?” Then I proofread my son’s essay, move the laundry, let the dogs out again, empty the dishwasher, eat a little food and go to bed. Alarm set for 5:30 to do it all again."},
+    { header: "", content: "Nursing school does a wonderful job preparing us to critically think, know how to perform tasks, and understand the processes of the body. I believe our schools can do a better job teaching the softer skills of nursing: how to stay fit, keep oneself well, have difficult conversations, manage relationships, and deal with stress.\nI’m grateful to be a part of the world of ICU nursing. I love being an agent of care and compassion for my patients and their families during some of the hardest days of their lives. If you know a nurse, hug them today and ask them about some of the lesser-known skills they have mastered." }
+  ]
+}];
+
+
+
+
+
+
+
+
 app.get('/', function(req: Request, res: Response) {
   res.redirect('/blogs');
 });
@@ -29,9 +81,14 @@ app.get('/blogs/:id', function(req: Request<{ id: string }>, res: Response) {
   let id = Number(req.params.id);
   if (isNaN(id)) {
     res.status(404).send('Not Found');
-  } else {
-    res.render('blog', {id: id});
-  }
+    return;
+  };
+  let blog: Blog = blogs[2];
+  res.render('blog', { blog: blog });
+});
+
+app.get('/test', function(req: Request, res: Response) {
+  res.send(html);
 });
 
 app.listen(port, function() {
---
+++ src/public/data/test.md
@@ -0,0 +1,33 @@
+# The Skills Needed by ICU Nurses:  A Primer on What We Really Need to Know
+
+As an ICU nurse, I believe I’m paid to think. Otherwise, soon, a robot could do my job. The reality is that I am the person ensuring my patients survive the twelve hours of my shift. I notice the subtle changes that sound my nurse alarm far before any electronic monitor can. However, many days, I think there are several skills truly overlooked in nursing school.
+
+## Untying Knots
+In nursing school, I never thought for a minute that I would spend a large amount of my time untangling cords. The spaghetti mess created by the intertwined and jumbled cables, IV tubing, suction tubing, and more is overwhelming. Add to the monster-of-a-mess a patient’s cell phone charger and the call bell button, and now even attempting to turn the patient is a disaster waiting to happen.
+
+I can spend 15–20 precious minutes carefully untangling the pile of lines — IV tubing laid so nicely on top of everything and pulse oximeter perfectly set free only to have my patient (always connected to pressure tubing) indicate an urgent need to go to the bedside commode. How a stand and a 180 degree pivot can undo all of my hard work still continues to baffle me.
+
+## The Limbo
+Before every ICU shift, there should be a period of stretching. As soon as I finish getting report on my new patients, I’m sure to get a call that it is time to transport my patient for a CT scan. I limbo under the IV tubing to unplug the pump. A little side limbo under the suction tubing for the ever-popular attempt to attach the IV pumps to the bed pole. Then, a precarious lean to unplug the bed and to fish the call bell button out of the cranny. Add a squeeze back to the end of the bed, and it is time to roll. With a helpful respiratory therapist towing the “portable” vent and a transport professional that makes driving the monster-of-a-bed look easy, I perform the most magnificent limbo under the computer, just squeezing by with the balloon pump. Here we go!
+
+## Squats
+Any day with a CRRT (continuous renal replacement therapy) machine leaves me feeling sorer and stronger than after any day at the gym! The bags seem to get heavier and heavier as the hours roll forward through the shift. Lift the bag. Squeeze the bag. Feel the satisfying pop. Mix the solution. Squat. Remove the empty bag. Disconnect the line. Hang the bag. (Why are the plastic grommets so hard to penetrate?) Reconnect the line. Stand. Repeat. Repeat. Repeat…
+
+## Reframing — The Realists
+Sometimes doctors, physician’s assistants, and nurse practitioners in the ICU setting tend to be specialized and caring for dozens of patients on a given day. They are amazing professionals but are often looking at a somewhat narrow and hurried picture of a patient. They do not spend hours and hours with any given patient in the same way as an ICU nurse.
+
+Into the patient’s room, enter the nephrologist, “The kidney numbers are improving.” Then, the surgeon, “The surgery site is healing nicely.” And infectious disease, “The white blood cells are trending down.” Everyone walks out, and the nurse, hanging the fourth IV pressor as the patient continues to decline, “I don’t think your loved one is going to make it through the day. Let’s talk about what we just heard.”
+
+## Hermione’s Time Turner
+Twelve hours. Go! Two patients. One vent. One CRRT machine. One balloon pump. Eight IV drips. One feeding tube. One walking-talking patient. Six rounds of diarrhea requiring three complete linen changes. One bath. Three dressing changes. Assist with a central line insertion. A trip to CT. Three calls from family. Feeding one patient. One sheath removed — hold pressure for 30 minutes. No, make that 60 minutes. Wait, 85 minutes. Up to the commode. New policy, must stay within arm’s length. Code blue across the hall. Do I leave patient on commode? No, stay. Making a mental list, it’s been three and a half hours since I turned my other patient. I don’t think this arms-length policy is going to work. Help patient back to bed. Wish I could untangle the cords — not now. Find the right TV channel. Plug the patient’s phone charger in. Remember my meds are late.
+
+Check on code. Answer call bell for nurse in the code. Crush meds. Answer phone. Look for provider. Report lab value. Try to draw labs. Fail. Call phlebotomy. Get interrogated — did I ask another nurse to try — well no, they are still in a code. Family in the hall, I forgot the ice water. Family on the phone — they’ve been calling for 20 minutes. I can’t find their nurse. She’s doing CPR. Did I give those meds? Yes, I can call x-ray. The order’s not right. I can fix that. Patient needs to poop again. I still didn’t turn my other patient. Only 3 hours to go and not a single thing documented yet, and did I turn my patient?
+
+## Shake It Off
+At 6:00 my patient died. I cried with his wife as I hugged her neck. I carefully put his body into the body bag and called the morgue. We took care of him for three months. There was nothing more to do that would help.
+
+At 8:00 I walk in the door at home and shower. My family asks the question to determine how may day was, “How many poops did you clean today, mom?” Then I proofread my son’s essay, move the laundry, let the dogs out again, empty the dishwasher, eat a little food and go to bed. Alarm set for 5:30 to do it all again.
+
+Nursing school does a wonderful job preparing us to critically think, know how to perform tasks, and understand the processes of the body. I believe our schools can do a better job teaching the softer skills of nursing: how to stay fit, keep oneself well, have difficult conversations, manage relationships, and deal with stress.
+
+I’m grateful to be a part of the world of ICU nursing. I love being an agent of care and compassion for my patients and their families during some of the hardest days of their lives. If you know a nurse, hug them today and ask them about some of the lesser-known skills they have mastered.
